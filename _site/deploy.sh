34c602ba10bbabb4ef60566f1504e4529e918b62
w4-jamie <jamie@skelterlabs.com>
2018-10-19 16:40:55 +0900

    Update config and css

--- _site/deploy.sh
+++
@@ -1,1 +0,0 @@
-s3cmd sync --exclude-from .s3ignore ./ s3://kyte.travel --delete-removed
--- _site/run_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Warning: SimpleHTTPServer does not serve video files properly."
-python -m SimpleHTTPServer 8000
