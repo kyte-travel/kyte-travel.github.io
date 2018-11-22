5440f4cac97121a1c959d24a9a7bf1986d96069c
yjiq150 <yjiq150@gmail.com>
2018-11-22 16:03:49 +0900

    appspec added for AWS CodeDeploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,6 @@
 
 DEPLOY_URL=s3://dev.kyte.travel
 
-if [ "$DEPLOY_PHASE" = "prd" ]; then
-	DEPLOY_URL=s3://kyte.travel
-fi
-
 echo "deploying to $DEPLOY_URL"
 
 s3cmd sync --exclude-from .s3ignore _site/ $DEPLOY_URL --delete-removed
