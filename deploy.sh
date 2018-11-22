ae120f48d9c4b60fd9f653a0c40afdd4e90b99d3
yjiq150 <yjiq150@gmail.com>
2018-11-22 12:01:44 +0900

    deploy by phase script added

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-s3cmd sync --exclude-from .s3ignore _site/ s3://kyte.travel --delete-removed
+#!/bin/bash
+
+DEPLOY_URL=s3://dev.kyte.travel
+
+if [ "$DEPLOY_PHASE" = "prd" ]; then
+	DEPLOY_URL=s3://kyte.travel
+fi
+
+echo "deploying to $DEPLOY_URL"
+
+s3cmd sync --exclude-from .s3ignore _site/ $DEPLOY_URL --delete-removed
