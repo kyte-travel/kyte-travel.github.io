3c0da9887c68c97caf9e72562641573cfa773de3
Jamie <jamie@squarelab.co>
2019-10-22 10:44:34 +0900

    Add video.css & video.js

---
+++ js/video.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  scaleVideoContainer();
+  initBannerVideoSize('.video-container .filter');
+  initBannerVideoSize('.video-container video');
+
+  $(window).on('resize', function () {
+    scaleVideoContainer();
+    scaleBannerVideoSize('.video-container .filter');
+    scaleBannerVideoSize('.video-container video');
+  });
+});
+
+function scaleVideoContainer() {
+  var height = Math.min($(window).height() + 5, 800);
+  var unitHeight = parseInt(height) + 'px';
+  $('.homepage-hero-module').css('height', unitHeight);
+}
+
+function initBannerVideoSize(element) {
+  $(element).each(function () {
+    $(this).data('height', $(this).height());
+    $(this).data('width', $(this).width());
+  });
+
+  scaleBannerVideoSize(element);
+}
+
+function scaleBannerVideoSize(element) {
+  var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+  $(element).each(function () {
+    var videoAspectRatio = $(this).data('height') / $(this).data('width');
+    $(this).width(windowWidth);
+    if (windowWidth < 768) {
+      videoHeight = windowHeight;
+      videoWidth = videoHeight / videoAspectRatio;
+      $(this).css({
+        'margin-top': 0,
+        'margin-left': -(videoWidth - windowWidth) / 2 + 'px'
+      });
+      $(this).width(videoWidth).height(videoHeight);
+    }
+    $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+  });
+}
